441c8d0d6f15920ca67c666cf62d62fae1b6c00c
HayanYoo <hayanyoo.dev@gmail.com>
2022-02-26 00:28:17 +0900

    feat: 이미지 크롤링 후 저장 기능

--- selenium/google.py
+++ selenium/google.py
@@ -9,7 +9,7 @@ elem = driver.find_element_by_name("q")
 elem.send_keys("조코딩")
 elem.send_keys(Keys.RETURN)
 
-SCROLL_PAUSE_TIME = 0.5
+SCROLL_PAUSE_TIME = 1.5
 
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
@@ -24,23 +24,28 @@ while True:
     # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
-        driver.find_element_by_css_selector(".mye4qd").click()
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
     last_height = new_height
-
-
 
 
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
 count = 1
 for image in images:
-    image.click()
-    time.sleep(3)
-    imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
-    opener=urllib.request.build_opener()
-    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-    urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-    count += 1
+    try:
+        image.click()
+        time.sleep(3)
+        imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img").get_attribute("src")
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count += 1
+    except Exception as e:
+        print(e)
+        pass
 
 # assert "Python" in driver.title
 # elem = driver.find_element_by_name("q")
@@ -48,4 +53,4 @@ for image in images:
 # elem.send_keys("pycon")
 # elem.send_keys(Keys.RETURN)
 # assert "No results found." not in driver.page_source
-#driver.close()
+driver.close()
