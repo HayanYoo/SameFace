8ce77abae05493d115c9e699002f51c6dca72300
HayanYoo <hayanyoo.dev@gmail.com>
2022-02-26 23:33:26 +0900

    안쓰는 코드 제거

--- selenium/google.py
+++ selenium/google.py
@@ -47,10 +47,4 @@ for image in images:
         print(e)
         pass
 
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
 driver.close()
