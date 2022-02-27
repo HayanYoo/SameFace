eda6ef778e8ceb75fd1f4f641dac38a25a5a2ebd
HayanYoo <hayanyoo.dev@gmail.com>
2022-02-27 13:50:55 +0900

    feat: 이미지 다운 시 폴더 생성 후 해당폴더에 저장

--- selenium/google.py
+++ selenium/google.py
@@ -2,49 +2,67 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
+import os
 
-driver = webdriver.Chrome()
-driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
-elem = driver.find_element_by_name("q")
-elem.send_keys("조코딩")
-elem.send_keys(Keys.RETURN)
 
-SCROLL_PAUSE_TIME = 1.5
 
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
+keywords = [  "BTS 정국"]
 
-while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+for keyword in keywords :
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
+    if not os.path.exists(keyword):
+        os.makedirs(keyword)
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
+
+
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys(keyword)
+    elem.send_keys(Keys.RETURN)
+
+    SCROLL_PAUSE_TIME = 1.5
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            try:
+                driver.find_element_by_css_selector(".mye4qd").click()
+            except:
+                break
+        last_height = new_height
+
+
+    images = driver.find_elements_by_css_selector("img.rg_i.Q4LuWd")
+    count = 1
+    for image in images:
         try:
-            driver.find_element_by_css_selector(".mye4qd").click()
-        except:
-            break
-    last_height = new_height
+            if count == 200: 
+                break
 
+            image.click()
+            time.sleep(2)
+            imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img").get_attribute("src")
+            opener=urllib.request.build_opener()
+            opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+            urllib.request.install_opener(opener)
+            path = keyword + "\\" + str(count) + ".jpg"
+            print(path)
+            urllib.request.urlretrieve(imgUrl, path)
+            print("save")
+            count += 1
+        except Exception as e:
+            print(e)
+            pass
 
-images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-count = 1
-for image in images:
-    try:
-        image.click()
-        time.sleep(3)
-        imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img").get_attribute("src")
-        opener=urllib.request.build_opener()
-        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-        urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-        count += 1
-    except Exception as e:
-        print(e)
-        pass
-
-driver.close()
+    driver.close()
